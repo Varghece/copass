3eb6a77b0f1acf6cf7174ecb5531a7e5fd4ffd00
Varghese <varghece@gmail.com>
2021-05-29 09:30:31 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AssignedCrewComponent } from './assigned-crew/assigned-crew.component';
+
+import { CheckboxModule } from 'primeng/checkbox';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignedCrewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/assigned-crew/assigned-crew.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assigned-crew',
+  templateUrl: './assigned-crew.component.html',
+  styleUrls: ['./assigned-crew.component.scss']
+})
+export class AssignedCrewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
