7f65c59456e1b6fa1a45ac9650431636588718dc
Varghese <varghece@gmail.com>
2021-06-02 23:17:56 +0530

    1003modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AssignedCrewComponent } from './assigned-crew/assigned-crew.component';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { BasicInformarionComponent } from './basic-informarion/basic-informarion.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { PancardComponent } from './pancard/pancard.component';
+import { AadharCardComponent } from './aadhar-card/aadhar-card.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AssignedCrewComponent
+    AssignedCrewComponent,
+    HeaderNavComponent,
+    BasicInformarionComponent,
+    NewCustomerComponent,
+    PancardComponent,
+    AadharCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/basic-informarion/basic-informarion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-informarion',
+  templateUrl: './basic-informarion.component.html',
+  styleUrls: ['./basic-informarion.component.scss']
+})
+export class BasicInformarionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
